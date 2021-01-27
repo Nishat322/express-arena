91466502ec5b37cfa1c119992650129a4126133c
Nishat Hoque <hoquen0397@gmail.com>
2021-01-27 16:17:57 -0500

    basic server

--- app.js
+++ app.js
@@ -8,3 +8,11 @@ const app = express();
 app.get('/', (req,res) => {
     res.send('Hello Express!');
 });
+
+//assigns a port for the server to be listening at 
+app.listen(8080, () => {
+    console.log('Express server is listening on port 8080!');
+});
+
+//npm start --> logs Express server is listening on port 8080! to terminal 
+//and Hello Express! to browser

bc6756127b1d3ea5cf9010c7f4eb5436992b4d96
Nishat Hoque <hoquen0397@gmail.com>
2021-01-27 17:20:45 -0500

    cipher assignment

--- app.js
+++ app.js
@@ -81,9 +81,53 @@ app.get('/sum', (req,res) => {
     }
 
     const sum = a + b;
-    res.send(`The sum of ${a} and ${b} is ${sum}`);
+    res.status(200).send(`The sum of ${a} and ${b} is ${sum}`);
 });
 
+app.get('/cipher', (req,res) => {
+    const {text,shift} = req.query;
+    if(!text) {
+        return res.status(400).send('text is required');
+    }
+    
+    if(!shift) {
+        return res.status(400).send('shift is required');
+    }
+    
+    const numShift = parseFloat(shift);
+    
+    if(Number.isNaN(numShift)) {
+        return res.status(400).send('shift must be a number');
+    }
+
+    const base = 'A'.charCodeAt(0);  // get char code 
+
+    const cipher = text
+                    .toUpperCase()
+                    .split('') // create an array of characters
+                    .map(char => { // map each original char to a converted char
+                        const code = char.charCodeAt(0); //get the char code
+                    // if it is not one of the 26 letters ignore it
+                        if(code < base || code > (base + 26)) {
+                        return char;
+                    }
+    // otherwise convert it
+    // get the distance from A
+    let diff = code - base;
+    diff = diff + numShift; 
+      
+    // in case shift takes the value past Z, cycle back to the beginning
+    diff = diff % 26;
+
+    // convert back to a character
+    const shiftedChar = String.fromCharCode(base + diff);
+    return shiftedChar;
+    })
+    .join(''); // construct a String from the array
+
+    // Return the response
+    res.status(200).send(cipher);  
+});
 
 
 
